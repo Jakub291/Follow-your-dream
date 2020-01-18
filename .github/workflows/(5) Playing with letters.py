d50658b0ec3408dfd4e1bc7498c64bfae889d689
Jakub291 <59981659+Jakub291@users.noreply.github.com>
2020-01-18 12:49:49 +0100

    Add files via upload

---
+++ .github/workflows/(5) Playing with letters.py
@@ -0,0 +1,4 @@
+sentence  = input('napisz zdanie') #write sentence
+
+print(len(sentence))
+print(sentence[:1] + "w" + sentence[2:].capitalize()) #change second sign to 'w' and capitalize third letter
