2c726d4411044d5003598804a83d57702233170a
Jakub291 <59981659+Jakub291@users.noreply.github.com>
2020-01-18 12:59:35 +0100

    Add files via upload

---
+++ .github/workflows/(9) age checking.py
@@ -0,0 +1,11 @@
+wiek = int(input ("ile masz lat"))
+x = 18 - wiek
+
+if (wiek < 18):
+    print ("niepełnoletni wróć za", x, "lat")
+elif (wiek > 100):
+    print ("200 lat!!")
+else:
+    print ("acces granted")
+
+end = input('nacisnij enter by skaczyc')
