d2975579bb31fde6ca42baa43e592701827eb366
Jakub291 <59981659+Jakub291@users.noreply.github.com>
2020-01-18 12:53:30 +0100

    Add files via upload

---
+++ .github/workflows/(7) weight converter.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+
+a = int(input('podaj stan wagi w gramach')) # write your weight in gram
+b = a/1000
+c = a/1000000
+print ('twoja waga w kg to: {}, a w tonach {}' .format(b , c) )
+
+f = input('press enter to end')
+
